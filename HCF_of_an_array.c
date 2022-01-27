5ba8da812f00fa163383d4d05321f99cff876ff0
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-01-27 21:51:51 +0530

    #include<stdio.h> int main() {
    int n,x[20],i,j,min,found=0,hcf=1;
    scanf("%d",&n);
    
    
    
    for(i=0;i<n;i++)
    {
    scanf("%d",&x[i]);
    }
    
    
    
    min=x[0];
    for(i=1;i<n;i++)
    {
    if(x[i]<min)
    {
    min=x[i];
    }
    }
    
    
    
    /*for(i=0,j=min;i<n && j>=1;i++,j--)
    {
    if(j % x[i]==0)
    {
    printf("%d",j);
    
    }
    
    
    
    }*/
    for(i=min;i>=1;i--)
    { found=0;
    for(j=0;j<n;j++)
    {
    if(x[j]%i!=0)
    {
    found=1;
    break;
    }
    }
    if(found==0)
    {
    hcf=i;
    break;
    }
    }
    printf("%d",hcf);
    
    
    
    return 0;
    
    
    
    }

---
+++ HCF_of_an_array.c
@@ -0,0 +1,66 @@
+
+#include<stdio.h>
+int main()
+{
+
+
+
+int n,x[20],i,j,min,found=0,hcf=1;
+scanf("%d",&n);
+
+
+
+for(i=0;i<n;i++)
+{
+scanf("%d",&x[i]);
+}
+
+
+
+min=x[0];
+for(i=1;i<n;i++)
+{
+if(x[i]<min)
+{
+min=x[i];
+}
+}
+
+
+
+/*for(i=0,j=min;i<n && j>=1;i++,j--)
+{
+if(j % x[i]==0)
+{
+printf("%d",j);
+
+}
+
+
+
+}*/
+for(i=min;i>=1;i--)
+{ found=0;
+for(j=0;j<n;j++)
+{
+if(x[j]%i!=0)
+{
+found=1;
+break;
+}
+}
+if(found==0)
+{
+hcf=i;
+break;
+}
+}
+printf("%d",hcf);
+
+
+
+return 0;
+
+
+
+}
