b98f63dbf17f970fb583ca6593f5c52810ab87c8
Sagar449102 <96296233+Sagar449102@users.noreply.github.com>
2022-02-14 20:41:24 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch<='A' && ch>='Z')     {         printf("%d",ch);     }     else     {         printf("%d",ch);     }     return 0; }

---
+++ Find_ASCII_Value_of_a_Character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch<='A' && ch>='Z')
+    {
+        printf("%d",ch);
+    }
+    else
+    {
+        printf("%d",ch);
+    }
+    return 0;
+}
